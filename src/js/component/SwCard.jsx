91970a5c8659f4519ec3010348a92a3c0dadc1a9
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-11-20 08:34:49 +0000

    1st- Card effect done

--- src/js/component/SwCard.jsx
+++ src/js/component/SwCard.jsx
@@ -10,7 +10,16 @@ export const SwCard = () => {
             </div>
             <div className="content">
                 <div className="details">
-                    <h2>Luck SkyWalker <br></br><span>Master Jedi</span></h2>
+                    <h2> Luck SkyWalker <br></br><span> Master Jedi </span></h2>
+                    <div className="data">
+                        <h3>342<br/><span> Post </span></h3>
+                        <h3>150k<br/><span> Followers </span></h3>
+                        <h3>333<br/><span> Following </span></h3>
+                    </div>
+                    <div className="actionBtn">
+                        <button> More... </button>
+                        <button> Favorites </button>
+                    </div>
 
                 </div>
             </div>
